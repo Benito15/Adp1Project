0adbdcbcb500bdc0af5ee95063f0f6596356caa8
Benitp <b.kriel34@gmail.com>
2019-02-14 02:57:59 +0200

    commit new changes to editBranch

--- src/main/java/ac/za/cput/App.java
+++ src/main/java/ac/za/cput/App.java
@@ -1,5 +1,7 @@
 package ac.za.cput;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -8,8 +10,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Hiiiiiiiiiiiiiiii");
-        
+    System.out.println( "Hello World!" );
+    System.out.println("Hiiiiiiiiiiiiiiii");
+
+    System.out.println("------------------");
+    System.out.println("Beginning with new Code in the editBranch");
+        ArrayList<String> colours = new ArrayList<>();
+
+        colours.add("red");
+        colours.add("green");
+        colours.add("yellow");
+        colours.add("white");
+        colours.add("neon");
+        colours.add("blue");
+        colours.add("purple");
+        colours.add("black");
+        colours.add("grey");
+        colours.add("silver");
+
+        System.out.println("Colours been added to arrayList");
+        System.out.println("-------------------------");
+
+        for (String i: colours)
+        {
+            System.out.println(i);
+        }
+
+
     }
 }
